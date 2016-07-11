3a03aeaaf67bc7d89f833dccc543843a3744e123
Junho Kim <kuh9325@naver.com>
2016-07-11 17:29:15 +0900

    자바스크립트 수정 및 이름 전달 구현 현재 3개 이상 카드 선택시 오류 메시지 출력 에러 존재 경고문 2종류를 모두 출력해야 해당 카드가 정상 기능 수행 선택한 카드의 번호를 넘기는 로직 구현해야 함 변수를 전체적으로 전역 변수로 선언하였음

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,42 +1,33 @@
 class HomeController < ApplicationController
   def index
-  end
 
-  def user
-    user = User.new
-    user.name = params[:name]
-    user.year = params[:year]
-    user.month = params[:month]
-    user.date = params[:date]
   end
 
   def selection
-    @card_list = ["death","judgement","justice","temperance",
+    $name = params[:name]
+    result
+
+    $card_list = ["death","judgement","justice","temperance",
       "the chariot","the devil","the emperor","the empress",
       "the fool","the hanged man","the hermit","the hierophant",
       "the high priestess","the lovers","the magician","the moon",
       "the star","the strength","the sun","the tower",
       "the world","wheel of fortune"]
-    @card = @card_list.sample(22).sort # 배열에서 비복원추출한 내용을 변수에 저장, (n)개의 표본 추출
-    @image_ = Array.new()
+      result
+    $card = $card_list.sample(22).sort # 배열에서 비복원추출한 내용을 변수에 저장, (n)개의 표본 추출
+    result
+    $image_ = Array.new()
+    result
 
     for i in 0..21
-      @image_[i] = @card[i] + ".png"
+      $image_[i] = $card[i] + ".png"
+      result
     end
 
   end
 
   def result
-    @card_list = ["death","judgement","justice","temperance",
-      "the chariot","the devil","the emperor","the empress",
-      "the fool","the hanged man","the hermit","the hierophant",
-      "the high priestess","the lovers","the magician","the moon",
-      "the star","the strength","the sun","the tower",
-      "the world","wheel of fortune"]
-    @card = @card_list.sample(3).sort # 배열에서 비복원추출한 내용을 변수에 저장, (n)개의 표본 추출
-    @image_1 = @card[0] + ".png"
-    @image_2 = @card[1] + ".png"
-    @image_3 = @card[2] + ".png"
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
-  get 'home/selection'
+  post 'home/selection'
   get 'home/result'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
